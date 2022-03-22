edf4fbfaa9c36adb9d4aced9ee12a1c26d035a6e
Rasujon3 <rasujon3@gmail.com>
2022-03-22 23:04:30 +0600

    49-4 Setting the cart data on the localStorage for future retrieve

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -14,16 +14,18 @@ const Cart = (props) => {
     shipping = shipping + product.shipping;
   }
 
-  const tax = (total * 0.1).toFixed(2);
+  const tax = parseFloat((total * 0.1).toFixed(2));
+
+  const grandTotal = total + shipping + tax;
 
   return (
     <div className="cart">
       <h4>Order Summary</h4>
       <p>Selected Items: {cart.length}</p>
       <p>Total Price: ${total}</p>
-      <p>Total Shipping: {shipping}</p>
-      <p>Tax: {tax}</p>
-      <h5>Grand Total: </h5>
+      <p>Total Shipping: ${shipping}</p>
+      <p>Tax: ${tax}</p>
+      <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
     </div>
   );
 };
