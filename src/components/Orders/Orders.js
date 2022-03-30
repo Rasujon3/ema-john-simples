eaf9cf5a1152b2fc23c43414ab5c0232c676e1a1
Rasujon3 <rasujon3@gmail.com>
2022-03-31 03:38:04 +0600

    53-9 Module Summary, use navigate and recap custom hook

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import useCart from "../../hooks/useCart";
 import useProducts from "../../hooks/useProducts";
 import { removeFromDb } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import ReviewItem from "../ReviewItem/ReviewItem";
+import "./Orders.css";
 
 const Orders = () => {
   const [products, setProducts] = useProducts();
   const [cart, setCart] = useCart(products);
+
+  const navigate = useNavigate();
 
   const handleRemoveProduct = (product) => {
     const rest = cart.filter((pd) => pd.id !== product.id);
@@ -30,7 +33,9 @@ const Orders = () => {
       <div className="cart-container">
         <Cart cart={cart}>
           <Link to="/inventory">
-            <button>Proceed Checkout</button>
+            <button onClick={() => navigate("/inventory")}>
+              Proceed Checkout
+            </button>
           </Link>
         </Cart>
       </div>
